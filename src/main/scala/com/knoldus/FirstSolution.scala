28952000bcb29d7971b2d30a058059da386ea1ee
Krishna Singh <krishna.singh9926@gmail.com>
2020-02-25 16:42:16 +0530

    Updated Source code

--- src/main/scala/com/knoldus/FirstSolution.scala
+++ src/main/scala/com/knoldus/FirstSolution.scala
@@ -1,17 +1,23 @@
 package com.knoldus
 
 object FirstSolution {
-
+  /**
+   * Figure class is an abstract class that has a basic implementation for figures such as Rectangle and Square
+   */
   abstract class Figure {
     def area(height: Double, width: Double): Double
   }
-
+  /**
+   * Rectangle extends Figure and overrides the method area
+   */
   class Rectangle extends Figure {
     override def area(height: Double, width: Double): Double = {
       height * width
     }
   }
-
+  /**
+   * Square extends Rectangle and overrides the method area and calculates area for square and also handles the calculation of area of rectangles
+   */
   class Square extends Rectangle {
     override def area(height: Double, width: Double): Double = {
       if (height != width) super.area(height, width) else height * height
--- src/main/scala/com/knoldus/SecondSolution.scala
+++ src/main/scala/com/knoldus/SecondSolution.scala
@@ -2,14 +2,23 @@ package com.knoldus
 
 object SecondSolution {
 
+  /**
+   * Figure class is an abstract class that has a basic implementation for figures such as Rectangle and Square
+   */
   abstract class Figure {
     def area(height: Double, width: Double): Double
   }
 
+  /**
+   * Rectangle extends Figure and overrides the method area
+   */
   class Rectangle extends Figure {
     override def area(height: Double, width: Double): Double = height * width
   }
 
+  /**
+   * Square extends Rectangle and overrides the method area and calculates area for square and also handles the calculation of area of rectangles
+   */
   class Square extends Rectangle {
     override def area(height: Double, width: Double): Double = {
       if (width == 0) height else area(height + height, width - 1)
